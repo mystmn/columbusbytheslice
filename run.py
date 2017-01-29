b108ced773c5c2244bfdc4a722b6ca7f1e40dfc7
pacman <pcameron5@gmail.com>
2017-01-29 17:34:31 -0500

    Added template, pictures

--- run.py
+++ run.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     name = ""
-    return render_template('home.html', name=name)
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
+@app.route('/pizza')
+def beta():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
