2a580c6bff52690a221002d4ab7cec0a3c34a796
pacman <pcameron5@gmail.com>
2017-01-28 21:01:08 -0500

    Routing

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+#  https://deliciouslyella.com/videos/
+
+@app.route('/')
+def index():
+    name = ""
+    return render_template('home.html', name=name)
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
