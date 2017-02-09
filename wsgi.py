4a6c70210abb9f02f612b4c3c98ef73f9b09d120
pacman <pcameron5@gmail.com>
2017-02-09 07:38:22 -0500

    functional ./videos

--- wsgi.py
+++ wsgi.py
@@ -13,6 +13,6 @@ from run import app as application
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 5000, application)
+    httpd = make_server('localhost', 5001, application)
     # Wait for a single request, serve it and quit.
     httpd.serve_forever()
